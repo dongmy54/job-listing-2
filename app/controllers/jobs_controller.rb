d653ae3097dc9c5681a0b2c8172743a886a1ea15
dongmy54 <615760433@qq.com>
2017-01-20 17:47:47 +0800

    Exchange jobs and admin under the index file

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -10,15 +10,15 @@ def show
 end
 
 def index
-	@jobs = case params[:order]
-	when 'by_lower_bound'
-		Job.published.order('wage_lower_bound DESC')
-    when  'by_upper_bound'
-        Job.published.order('wage_upper_bound DESC')
-    else
-    	Job.published.recent
-    end
-end
+    @jobs = case params[:order]
+            when 'by_lower_bound'
+              Job.published.order('wage_lower_bound DESC')
+            when 'by_upper_bound'
+              Job.published.order('wage_upper_bound DESC')
+            else
+              Job.published.recent
+            end
+  end
 
 def new
 	@job = Job.new
